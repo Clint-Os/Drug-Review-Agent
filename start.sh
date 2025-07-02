6081fb1fdb4244024a06784834bbfeb6034ca6a0
Clint-Os <dwayneqlint1@gmail.com>
2025-07-02 20:05:28 +0300

    Fix:update start.sh file to run streamlit

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 # Start FastAPI (api.py) in the background
 uvicorn api:app --host 0.0.0.0 --port 8000 &
 
-# Start Streamlit frontend (ui.py)
-streamlit streamlit_app.py --server.port 8501 --server.address=0.0.0.0
+# Start Streamlit frontend 
+streamlit run streamlit_app.py --server.port 8501 --server.address=0.0.0.0
